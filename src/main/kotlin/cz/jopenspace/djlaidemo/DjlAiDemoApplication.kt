3a89723a200c8672e993f838175dc9c76bac03fc
SpiReCZ <SpiReCZ@users.noreply.github.com>
2023-10-05 17:33:20 +0200

    init project

---
+++ src/main/kotlin/cz/jopenspace/djlaidemo/DjlAiDemoApplication.kt
@@ -0,0 +1,11 @@
+package cz.jopenspace.djlaidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DjlAiDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DjlAiDemoApplication>(*args)
+}
---
+++ src/test/kotlin/cz/jopenspace/djlaidemo/DjlAiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.jopenspace.djlaidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DjlAiDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
